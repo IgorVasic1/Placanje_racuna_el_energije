ec9c13ae09a27508acaeb54bb3e9ac1f3078743f
IgorVasic1 <80054412+IgorVasic1@users.noreply.github.com>
2022-06-18 19:38:41 +0200

    Update GenerateBill.java
    Mala promjena

--- src/electricity/billing/system/GenerateBill.java
+++ src/electricity/billing/system/GenerateBill.java
@@ -40,7 +40,7 @@ public class GenerateBill extends JFrame implements ActionListener{
 
         
         t1 = new JTextArea(50,15);
-        t1.setText("\n\n\t------- Click on the -------\n\t Generate Bill Button to get\n\tthe bill of the Selected Month\n\n");
+        t1.setText("\n\n\t------- klikni na  -------\n\tGeneriranje računa da biste dobili \n\tračun za odabrani mjesec\n\n");
         JScrollPane jsp = new JScrollPane(t1);
         t1.setFont(new Font("Senserif",Font.ITALIC,18));
         
@@ -68,13 +68,13 @@ public class GenerateBill extends JFrame implements ActionListener{
             ResultSet rs = c.s.executeQuery("select * from customer where meter="+meter);
             
             if(rs.next()){
-                t1.append("\n    Customer Name:"+rs.getString("name"));
-                t1.append("\n    Meter Number:  "+rs.getString("meter"));
-                t1.append("\n    Address:            "+rs.getString("address"));
-                t1.append("\n    State:                 "+rs.getString("state"));
-                t1.append("\n    City:                   "+rs.getString("city"));
-                t1.append("\n    Email:                "+rs.getString("email"));
-                t1.append("\n    Phone Number  "+rs.getString("phone"));
+                t1.append("\n    Ime potrošača:"+rs.getString("name"));
+                t1.append("\n    ID brojila:  "+rs.getString("meter"));
+                t1.append("\n    Adresa Stanovanja:            "+rs.getString("address"));
+                t1.append("\n    Država:                 "+rs.getString("state"));
+                t1.append("\n    Grad:                   "+rs.getString("city"));
+                t1.append("\n    Email adresa:                "+rs.getString("email"));
+                t1.append("\n    Broj telefona:  "+rs.getString("phone"));
                 t1.append("\n-------------------------------------------------------------");
                 t1.append("\n");
             }
@@ -82,23 +82,23 @@ public class GenerateBill extends JFrame implements ActionListener{
             rs = c.s.executeQuery("select * from meter_info where meter_number = " + meter);
             
             if(rs.next()){
-                t1.append("\n    Meter Location:"+rs.getString("meter_location"));
-                t1.append("\n    Meter Type:      "+rs.getString("meter_type"));
-                t1.append("\n    Phase Code:    "+rs.getString("phase_code"));
-                t1.append("\n    Bill Type:         "+rs.getString("bill_type"));
-                t1.append("\n    Days:               "+rs.getString("days"));
+                t1.append("\n    Lokacija brojila:"+rs.getString("meter_location"));
+                t1.append("\n    Tip brojila:      "+rs.getString("meter_type"));
+                t1.append("\n    Šifra osnovice:    "+rs.getString("phase_code"));
+                t1.append("\n    Vrsta računa:         "+rs.getString("bill_type"));
+                t1.append("\n    Broj dana:               "+rs.getString("days"));
                 t1.append("\n");
             }
             rs = c.s.executeQuery("select * from tax");
             if(rs.next()){
                 t1.append("---------------------------------------------------------------");
                 t1.append("\n\n");
-                t1.append("\n Cost per Unit:             Rs "+rs.getString("cost_per_unit"));
-                t1.append("\n Meter Rent:                Rs "+rs.getString("meter_rent"));
-                t1.append("\n Service Charge:            Rs "+rs.getString("service_charge"));
-                t1.append("\n Service Tax:               Rs "+rs.getString("service_tax"));
-                t1.append("\n Swacch Bharat Cess:        Rs "+rs.getString("swacch_bharat_cess"));
-                t1.append("\n Fixed Tax:                 Rs "+rs.getString("fixed_tax"));
+                t1.append("\n Cijena po jedinici:             Rs "+rs.getString("cost_per_unit"));
+                t1.append("\n Marža:                Rs "+rs.getString("meter_rent"));
+                t1.append("\n Naplata po usluzi:            Rs "+rs.getString("service_charge"));
+                t1.append("\n Porez usluge:               Rs "+rs.getString("service_tax"));
+                t1.append("\n Prirez:        Rs "+rs.getString("swacch_bharat_cess"));
+                t1.append("\n Porez na račun:                 Rs "+rs.getString("fixed_tax"));
                 t1.append("\n");
                 
             }
@@ -106,11 +106,11 @@ public class GenerateBill extends JFrame implements ActionListener{
             rs = c.s.executeQuery("select * from bill where meter="+meter+" AND month = '"+c2.getSelectedItem()+"'");
             
             if(rs.next()){
-                t1.append("\n    Current Month :\t"+rs.getString("month"));
-                t1.append("\n    Units Consumed:\t"+rs.getString("units"));
-                t1.append("\n    Total Charges :\t"+rs.getString("total_bill"));
+                t1.append("\n    Tekući mjesec :\t"+rs.getString("month"));
+                t1.append("\n    Ukupna potrošnja po jedinici:\t"+rs.getString("units"));
+                t1.append("\n    Ukupna naplata :\t"+rs.getString("total_bill"));
                 t1.append("\n---------------------------------------------------------------");
-                t1.append("\n    TOTAL PAYABLE :\t"+rs.getString("total_bill"));
+                t1.append("\n    UKUPAN RAČUN :\t"+rs.getString("total_bill"));
             }
             
             
